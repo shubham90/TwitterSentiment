652c1e1a56a2bd942a5082d3e647637e52bb6df0
shubham90 <shubham.bhawsinka@gmail.com>
2015-06-13 18:59:02 -0400

    adding data monitor

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['TwitterSentiment'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '768cad1da833c9b0fd1f8b85aef8de29177dbdc6',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
